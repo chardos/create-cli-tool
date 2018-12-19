1df16e864b172e3583e37c3f8f9fa3e69d21a5ef
Richard Tan <chardos@gmail.com>
2018-12-19 21:03:42 +1100

    make-cli-util

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
-const minimist = require('minimist');
-const findUp = require('find-up');
-const CONFIG_FILE_NAME = '.fsm.js';
-
 module.exports = async () => {
-  const args = minimist(process.argv.slice(2));
-  const { 
-    _: commands,
-    ...options
-  } = args;
-  
-  const command = commands[0];
-
-  console.log('command', command);
-
-  return await findUp(CONFIG_FILE_NAME)
-    // .then(configPath => ({ configPath, command, positionalArgs, options }))
-    // .then(warnMissingDependencies)
-    // .then(getConfig)
-    // .then(runValidations)
-    // .then(decorateData)
-    // .then(parseCommand)
-    // .catch(logger.error);
+  console.log('Created by make-cli-tool');
+  console.log('process.argv', process.argv);
 };
