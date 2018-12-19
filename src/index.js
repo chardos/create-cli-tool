392fea8ff5adad393f99d24a1003e43ea277741d
Richard Tan <chardos@gmail.com>
2018-12-20 06:44:21 +1100

    Making directory

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
+const makeDir = require('make-dir');
+const minimist = require('minimist');
+
 module.exports = async () => {
-  console.log('Created by make-cli-tool');
-  console.log('process.argv', process.argv);
+  const args = minimist(process.argv.slice(2));
+  const { 
+    _: positionalArgs,
+    ...options
+  } = args;
+  const [name] = positionalArgs;
+
+  // write to cwd
+  console.log('process.cwd()', process.cwd());
+
+  //create the folder
+  makeDir(`${process.cwd()}/${name}`);
+  
 };
